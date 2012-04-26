e7c9b1ecb61aab8958a74341a64fcbd7acde9492
Matthias Wimmer <m@tthias.eu>
2012-04-26 16:49:24 +0200

    Update LRC-Trainer to version 1.1 (2).

--- LRC-Trainer/src/eu/wimmerinformatik/lrc/data/Repository.java
+++ LRC-Trainer/src/eu/wimmerinformatik/lrc/data/Repository.java
@@ -27,7 +27,7 @@ public class Repository extends SQLiteOpenHelper {
 	private static final int NUMBER_LEVELS = 5;
 
 	public Repository(final Context context) {
-		super(context, "topics", null, 1);
+		super(context, "topics", null, 2);
 		this.context = context;
 	}
 
@@ -300,8 +300,10 @@ public class Repository extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-
+		if (oldVersion <= 1) {
+			final ContentValues updates = new ContentValues();
+			updates.put("question", "Welches Funkzeugnis ist mindestens erforderlich, um mit einer Seefunkstelle auf einem Sportfahrzeug am Weltweiten Seenot- und Sicherheitsfunksystem (GMDSS) im Seegebiet A3 teilnehmen zu können?");
+			db.update("question", updates, "_id=?", new String[]{"4408"});
+		}
 	}
-
 }
