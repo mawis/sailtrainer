1a07434503d6b0790a7868898378fbb0db0e621b
Matthias Wimmer <m@tthias.eu>
2012-08-26 23:46:32 +0200

    Removing unused import.

--- SKS-Trainer/src/eu/wimmerinformatik/sks/QuestionAsker.java
+++ SKS-Trainer/src/eu/wimmerinformatik/sks/QuestionAsker.java
@@ -16,7 +16,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
