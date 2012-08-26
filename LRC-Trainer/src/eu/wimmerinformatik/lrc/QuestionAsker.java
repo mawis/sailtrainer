644bff9f0ff74aba90fe5f958b5e62d5ef10b790
Matthias Wimmer <m@tthias.eu>
2012-08-27 01:22:46 +0200

    Changes for next version of LRC except showing the current level of a question.

--- LRC-Trainer/src/eu/wimmerinformatik/lrc/QuestionAsker.java
+++ LRC-Trainer/src/eu/wimmerinformatik/lrc/QuestionAsker.java
@@ -22,6 +22,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -41,6 +42,8 @@ import android.widget.Toast;
 public class QuestionAsker extends Activity {
 	private Repository repository;
 	private int currentQuestion;
+	private int maxProgress;
+	private int currentProgress;
 	private int topicId;
 	private int correctChoice;
 	private Random rand = new Random();
@@ -51,6 +54,7 @@ public class QuestionAsker extends Activity {
 	private Date nextTime;
 	private Timer waitTimer;
 	private boolean showingStandardView;
+	private boolean replaceNNBSP = Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH;
 	
 	@Override
 	public void onDestroy() {
@@ -72,6 +76,8 @@ public class QuestionAsker extends Activity {
 		
 		outState.putBoolean(getClass().getName() + ".showingCorrectAnswer", showingCorrectAnswer);
 		outState.putInt(getClass().getName() + ".currentQuestion", currentQuestion);
+		outState.putInt(getClass().getName() + ".maxProgress", maxProgress);
+		outState.putInt(getClass().getName() + ".currentProgress", currentProgress);
 		outState.putLong(getClass().getName() + ".topic", topicId);
 		if (nextTime != null) {
 			outState.putLong(getClass().getName() + ".nextTime", nextTime.getTime());
@@ -103,6 +109,8 @@ public class QuestionAsker extends Activity {
         	final long nextTimeLong = savedInstanceState.getLong(getClass().getName()+".nextTime");
         	nextTime = nextTimeLong > 0L ? new Date(nextTimeLong) : null;
         	showingCorrectAnswer = savedInstanceState.getBoolean(getClass().getName()+".showingCorrectAnswer");
+			maxProgress = savedInstanceState.getInt(getClass().getName() + ".maxProgress");
+			currentProgress = savedInstanceState.getInt(getClass().getName() + ".currentProgress");
         	
         	final String orderString = savedInstanceState.getString(getClass().getName()+".order");
         	if (orderString != null) {
@@ -268,6 +276,8 @@ public class QuestionAsker extends Activity {
 		final int selectedQuestion = nextQuestion.getSelectedQuestion();
 		if (selectedQuestion != 0) {
 			currentQuestion = selectedQuestion;
+			maxProgress = nextQuestion.getMaxProgress();
+			currentProgress = nextQuestion.getCurrentProgress();
 			nextTime = null;
 			showQuestion();
 			return;
@@ -334,7 +344,7 @@ public class QuestionAsker extends Activity {
 
 		final TextView textView = (TextView) findViewById(R.id.textViewFrage);
         
-		textView.setText(question.getQuestionText());
+		textView.setText(safeText(question.getQuestionText()));
 		
 		final List<RadioButton> radioButtons = getRadioButtons();
 		
@@ -347,11 +357,12 @@ public class QuestionAsker extends Activity {
 		}
 		correctChoice = radioButtons.get(order.get(0)).getId();
 		for (int i = 0; i < 4; i++) {
-			radioButtons.get(order.get(i)).setText(question.getAnswers().get(i));
+			radioButtons.get(order.get(i)).setText(safeText(question.getAnswers().get(i)));
 		}
 
 		final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar1);
-		progressBar.setProgress(question.getLevel());
+		progressBar.setMax(maxProgress);
+		progressBar.setProgress(currentProgress);
 	}
 	
 	private void showNextQuestionAt(final Date when) {
@@ -380,5 +391,9 @@ public class QuestionAsker extends Activity {
 			waitTimer.cancel();
 			waitTimer = null;
 		}
+	}
+
+	private String safeText(final String source) {
+		return replaceNNBSP ? source.replace('\u202f', '\u00a0') : source;
 	}
 }
