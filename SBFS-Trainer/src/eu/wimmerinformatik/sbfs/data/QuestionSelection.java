65903fd74795e31daf699a821c854d3e7743e0b6
Matthias Wimmer <m@tthias.eu>
2012-08-29 19:59:26 +0200

    Same changes for SBFS.

--- SBFS-Trainer/src/eu/wimmerinformatik/sbfs/data/QuestionSelection.java
+++ SBFS-Trainer/src/eu/wimmerinformatik/sbfs/data/QuestionSelection.java
@@ -8,35 +8,49 @@ public class QuestionSelection {
 	private Date nextQuestion;
 	private int openQuestions;
 	private int totalQuestions;
+	private int maxProgress;
+	private int currentProgress;
+
 	public int getSelectedQuestion() {
 		return selectedQuestion;
 	}
-	public void setSelectedQuestion(int selectedQuestion) {
+	public void setSelectedQuestion(final int selectedQuestion) {
 		this.selectedQuestion = selectedQuestion;
 	}
 	public boolean isFinished() {
 		return finished;
 	}
-	public void setFinished(boolean finished) {
+	public void setFinished(final boolean finished) {
 		this.finished = finished;
 	}
 	public Date getNextQuestion() {
 		return nextQuestion;
 	}
-	public void setNextQuestion(Date nextQuestion) {
+	public void setNextQuestion(final Date nextQuestion) {
 		this.nextQuestion = nextQuestion;
 	}
 	public int getOpenQuestions() {
 		return openQuestions;
 	}
-	public void setOpenQuestions(int openQuestions) {
+	public void setOpenQuestions(final int openQuestions) {
 		this.openQuestions = openQuestions;
 	}
 	public int getTotalQuestions() {
 		return totalQuestions;
 	}
-	public void setTotalQuestions(int totalQuestions) {
+	public void setTotalQuestions(final int totalQuestions) {
 		this.totalQuestions = totalQuestions;
 	}
-
+	public int getMaxProgress() {
+		return maxProgress;
+	}
+	public void setMaxProgress(final int maxProgress) {
+		this.maxProgress = maxProgress;
+	}
+	public int getCurrentProgress() {
+		return currentProgress;
+	}
+	public void setCurrentProgress(final int currentProgress) {
+		this.currentProgress = currentProgress;
+	}
 }
