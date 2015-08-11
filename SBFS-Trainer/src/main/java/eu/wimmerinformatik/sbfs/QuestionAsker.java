d5e707b3a368933378d1dcdfcb3046b1d26c2688
Matthias Wimmer <m@tthias.eu>
2015-08-11 13:38:43 +0200

    Fixing stylesheet for correct answer in white layout screens (SBFS-Trainer).

--- SBFS-Trainer/src/main/java/eu/wimmerinformatik/sbfs/QuestionAsker.java
+++ SBFS-Trainer/src/main/java/eu/wimmerinformatik/sbfs/QuestionAsker.java
@@ -259,6 +259,7 @@ public class QuestionAsker extends Activity {
 					
 					final RadioButton correctButton = (RadioButton) findViewById(correctChoice);
 					correctButton.setBackgroundResource(R.color.correctAnswer);
+					correctButton.setTextAppearance(QuestionAsker.this, R.style.correctAnswerStyle);
 					
 					return;
 				} else {
@@ -276,7 +277,8 @@ public class QuestionAsker extends Activity {
 		if (correctChoice != 0) {
 			final RadioButton correctButton = (RadioButton) findViewById(correctChoice);
 			correctButton.setBackgroundResource(0);
-		}
+			correctButton.setTextAppearance(this, android.R.style.TextAppearance);
+	}
 		
 		order = null;
 		
