b8d28c7bbb3d3c7771f37bcd5a52b3678c7fa806
Matthias Wimmer <m@tthias.eu>
2017-09-09 19:24:27 +0200

    Updated the questions for the sea to the 2017 catalogue.

--- SBFS-Trainer/src/main/java/eu/wimmerinformatik/sbfs/data/Repository.java
+++ SBFS-Trainer/src/main/java/eu/wimmerinformatik/sbfs/data/Repository.java
@@ -1,5 +1,4 @@
-/*  vim: set sw=4 tabstop=4 fileencoding=UTF-8:
- *
+/*
  *  Copyright 2014 Matthias Wimmer
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -37,15 +36,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.SimpleCursorAdapter;
 
 public class Repository extends SQLiteOpenHelper {
+	private static final String QUESTION = "question";
+	private static final String QUESTION_QUERY = "_id=?";
+	private static final String ANSWER = "answer";
+	private static final String ANSWER_QUERY =
+		"question_id=? AND order_index=?";
+	private static final String CORRECT_ANSWER = "0";
+	private static final String FIRST_INCORRECT = "1";
+	private static final String SECOND_INCORRECT = "2";
+	private static final String THIRD_INCORRECT = "3";
+	private static final int NUMBER_LEVELS = 5;
+
 	private final Context context;
 	private int answerIdSeq;
 	private SQLiteDatabase database;
 	private final String done;
-	
-	private static final int NUMBER_LEVELS = 5;
 
 	public Repository(final Context context) {
-		super(context, "topics", null, 4);
+		super(context, "topics", null, 5);
 		done = context.getString(R.string.done);
 		this.context = context;
 	}
@@ -62,7 +70,7 @@ public class Repository extends SQLiteOpenHelper {
 		} finally {
 			db.endTransaction();
 		}
-				
+
 		// fill with data
 		try {
 			final List<Topic> topics = new LinkedList<Topic>();
@@ -121,7 +129,7 @@ public class Repository extends SQLiteOpenHelper {
 				eventType = xmlResourceParser.getEventType();
 			}
 			xmlResourceParser.close();
-			
+
 			db.beginTransaction();
 			try {
 				for (final Topic topic : topics) {
@@ -140,18 +148,18 @@ public class Repository extends SQLiteOpenHelper {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public QuestionSelection selectQuestion(final int topicId) {
 		final QuestionSelection result = new QuestionSelection();
 		final List<Integer> possibleQuestions = new LinkedList<Integer>();
 		final long now = new Date().getTime();
-		
+
 		int questionCount = 0;
 		int openQuestions = 0;
 		int maxProgress = 0;
 		int currentProgress = 0;
 		long soonestNextTime = 0;
-		
+
 		final Cursor c = getDb().query("question", new String[]{"_id", "level", "next_time"}, "topic_id=?", new String[]{Integer.toString(topicId)}, null, null, null, null);
 		try {
 			c.moveToNext();
@@ -159,29 +167,29 @@ public class Repository extends SQLiteOpenHelper {
 				final int questionId = c.getInt(0);
 				final int level = c.getInt(1);
 				final long nextTime = c.getLong(2);
-				
+
 				questionCount++;
 				maxProgress += NUMBER_LEVELS;
 				currentProgress += level;
 				if (level < NUMBER_LEVELS) {
 					openQuestions++;
-					
+
 					if (nextTime > now) {
 						if (soonestNextTime == 0 || soonestNextTime > nextTime) {
 							soonestNextTime = nextTime;
 						}
 					} else {
 						possibleQuestions.add(questionId);
-					}	
+					}
 				}
-				
+
 				c.moveToNext();
 			}
-			
+
 		} finally {
 			c.close();
 		}
-		
+
 		result.setTotalQuestions(questionCount);
 		result.setMaxProgress(maxProgress);
 		result.setCurrentProgress(currentProgress);
@@ -193,13 +201,13 @@ public class Repository extends SQLiteOpenHelper {
 		} else if (soonestNextTime > 0) {
 			result.setNextQuestion(new Date(soonestNextTime));
 		}
-		
+
 		return result;
 	}
-	
+
 	public Question getQuestion(final int questionId) {
 		final Question question = new Question();
-		
+
 		final Cursor c = getDb().query("question", new String[]{"_id", "topic_id", "reference", "question", "level", "next_time"}, "_id=?", new String[]{Integer.toString(questionId)}, null, null, null, null);
 		try {
 			c.moveToNext();
@@ -215,7 +223,7 @@ public class Repository extends SQLiteOpenHelper {
 		} finally {
 			c.close();
 		}
-		
+
 		// _id INT NOT NULL PRIMARY KEY, question_id INT NOT NULL REFERENCES question(id) ON DELETE CASCADE, order_index INT NOT NULL, answer TEXT
 		final Cursor answer = getDb().query("answer", new String[]{"answer"}, "question_id=?", new String[]{Integer.toString(questionId)}, null, null, "order_index");
 		try {
@@ -223,17 +231,17 @@ public class Repository extends SQLiteOpenHelper {
 			while (!answer.isAfterLast()) {
 				question.getAnswers().add(answer.getString(0));
 				answer.moveToNext();
-			}	
+			}
 		} finally {
 			answer.close();
 		}
-		
+
 		return question;
 	}
 
-    public Topic getTopic(final int topicId) {
+	public Topic getTopic(final int topicId) {
 		final Topic topic = new Topic();
-            
+
 		final Cursor c = getDb().query("topic", new String[]{"_id", "order_index", "name"}, "_id=?", new String[]{Integer.toString(topicId)}, null, null, null);
 		try {
 			c.moveToNext();
@@ -248,9 +256,9 @@ public class Repository extends SQLiteOpenHelper {
 		}
 
 		return topic;
-    }
+	}
 
-    public TopicStats getTopicStat(final int topicId) {
+	public TopicStats getTopicStat(final int topicId) {
 		final TopicStats stats = new TopicStats();
 		stats.setLevels(NUMBER_LEVELS);
 		stats.setQuestionsAtLevel(new int[NUMBER_LEVELS+1]);
@@ -278,55 +286,55 @@ public class Repository extends SQLiteOpenHelper {
 		stats.setQuestionCount(questionCount);
 
 		return stats;
-    }
-	
+	}
+
 	public void answeredCorrect(final int questionId) {
 		final Question question = getQuestion(questionId);
 		final int newLevel = question.getLevel() + 1;
-		
-		updateAnswered(questionId, newLevel);	
+
+		updateAnswered(questionId, newLevel);
 	}
-	
+
 	public void answeredIncorrect(final int questionId) {
 		final Question question = getQuestion(questionId);
 		final int newLevel = question.getLevel() <= 0 ? 0 : question.getLevel() - 1;
-		
-		updateAnswered(questionId, newLevel);		
+
+		updateAnswered(questionId, newLevel);
 	}
-	
+
 	public void continueNow(final int topicId) {
 		final ContentValues updates = new ContentValues();
 		updates.put("next_time", new Date().getTime());
 		getDb().update("question", updates, "topic_id=?", new String[]{Integer.toString(topicId)});
 	}
-	
+
 	public void resetTopic(final int topicId) {
 		final ContentValues updates = new ContentValues();
 		updates.put("next_time", new Date().getTime());
 		updates.put("level", 0L);
 		getDb().update("question", updates, "topic_id=?", new String[]{Integer.toString(topicId)});		
 	}
-	
+
 	public void setTopicsInSimpleCursorAdapter(final SimpleCursorAdapter adapter) {
 		final Cursor c = getTopicsCursor(getDb());
 		adapter.changeCursor(c);
 	}
-	
+
 	public Cursor getTopicsCursor(final SQLiteDatabase db) {
 		final Cursor cursor = db.rawQuery("SELECT t._id AS _id, t.order_index AS order_index, t.name AS name, CASE WHEN MIN(level) >= " + NUMBER_LEVELS + " THEN ? ELSE SUM(CASE WHEN level < " + NUMBER_LEVELS +" THEN 1 ELSE 0 END) END AS status, MIN(CASE WHEN level >= " + NUMBER_LEVELS + " THEN NULL ELSE next_time END) AS next_question FROM topic t LEFT JOIN question q ON q.topic_id = t._id GROUP BY t._id, t.order_index, t.name ORDER BY t.order_index", new String[]{done});
 		return cursor;
 	}
-	
+
 	private void updateAnswered(final int questionId, final int newLevel) {
 		final long newNextTime = new Date().getTime() + waitingTimeOnLevel(newLevel);
-		
+
 		final ContentValues updates = new ContentValues();
 		updates.put("level", newLevel);
 		updates.put("next_time", newNextTime);
-		
+
 		getDb().update("question", updates, "_id=?", new String[]{Integer.toString(questionId)});
 	}
-	
+
 	private long waitingTimeOnLevel(final int level) {
 		return level <= 0 ? 15000L :
 			level == 1 ? 60000L :
@@ -343,7 +351,7 @@ public class Repository extends SQLiteOpenHelper {
 		contentValues.put("name", currentTopic.getName());
 		db.insert("topic", null, contentValues);
 	}
-	
+
 	private void save(final SQLiteDatabase db, final Question question) {
 		final ContentValues contentValues = new ContentValues();
 		contentValues.put("_id", question.getId());
@@ -353,7 +361,7 @@ public class Repository extends SQLiteOpenHelper {
 		contentValues.put("level", 0);
 		contentValues.put("next_time", question.getNextTime().getTime());
 		db.insert("question", null, contentValues);
-			
+
 		int answerIndex = 0;
 		for (final String answer : question.getAnswers()) {
 			contentValues.clear();
@@ -364,7 +372,7 @@ public class Repository extends SQLiteOpenHelper {
 			db.insert("answer", null, contentValues);
 		}
 	}
-	
+
 	private SQLiteDatabase getDb() {
 		if (database == null) {
 			database = this.getWritableDatabase();
@@ -387,5 +395,97 @@ public class Repository extends SQLiteOpenHelper {
 			updates.put("answer", "Sportboote mit Antriebsmaschine mit einer größeren Nutzleistung als 11,03 Kilowatt (15 PS).");
 			db.update("answer", updates, "question_id=? AND order_index=?", new String[]{"9664", "2"});
 		}
+		if (oldVersion <= 4) {
+			updateQuestion(
+					db,
+					"9589",
+					"Was ist zu tun, wenn vor Antritt der Fahrt nicht feststeht, wer Schiffsführer ist?");
+			updateAnswer(
+					db,
+					"9589",
+					CORRECT_ANSWER,
+					"Der verantwortliche Schiffsführer muss bestimmt werden.");
+			updateAnswer(
+					db,
+					"9589",
+					FIRST_INCORRECT,
+					"Der verantwortliche Schiffsführer muss gewählt werden.");
+			updateAnswer(
+					db,
+					"9589",
+					SECOND_INCORRECT,
+					"Ein Inhaber eines Sportbootführerscheins muss die "
+					+ "Fahrzeugführung übernehmen.");
+			updateAnswer(
+					db,
+					"9589",
+					THIRD_INCORRECT,
+					"Ein Inhaber eines Sportbootführerscheins muss die "
+					+ "Verantwortung übernehmen.");
+
+			updateAnswer(
+					db,
+					"9674",
+					FIRST_INCORRECT,
+					"Die Kollisionsverhütungsregeln (KVR), die "
+					+ "Seeschifffahrtsstraßen-Ordnung (SeeSchStrO) und die "
+					+ "Sportbootführerscheinverordnung.");
+
+			updateAnswer(
+					db,
+					"9742",
+					FIRST_INCORRECT,
+					"Die Nachrichten für Seefahrer (NfS), herausgegeben vom "
+					+ "Bundesamt für Seeschifffahrt und Hydrographie, sowie "
+					+ "die Bekanntmachungen für Seefahrer (BfS) der örtlich "
+					+ "zuständigen Wasserstraßen- und Schifffahrtsämter, die "
+					+ "auf alle Veränderungen hinsichtlich Betonnung, Wracks "
+					+ "und Untiefen sowie auf die Schifffahrt betreffende "
+					+ "Maßnahmen und Ereignisse hinweisen.");
+			updateAnswer(
+					db,
+					"9742",
+					SECOND_INCORRECT,
+					"Die nautische Veröffentlichung „Sicherheit auf dem "
+					+ "Wasser, herausgegeben durch das Bundesministerium für "
+					+ "Verkehr und digitale Infrastruktur (BMVI), mit "
+					+ "wichtigen Regeln und Tipps für Wassersportler.");
+
+			updateAnswer(
+					db,
+					"9743",
+					SECOND_INCORRECT,
+					"Ergänzende Vorschriften für den NOK in der "
+					+ "Seeschifffahrtsstraßen-Ordnung sowie in der "
+					+ "Sportbootführerscheinverordnung.");
+
+			updateAnswer(
+					db,
+					"9814",
+					SECOND_INCORRECT,
+					"Befahrensregeln beachten sowie Wasserschutzpolizei und "
+					+ "Wasserstraßen- und Schifffahrtsamt informieren.");
+		}
+	}
+
+	private void updateQuestion(
+			final SQLiteDatabase db,
+			final String id,
+			final String text) {
+
+		final ContentValues update = new ContentValues();
+		update.put(QUESTION, text);
+		db.update(QUESTION, update, QUESTION_QUERY, new String[]{id});
+	}
+
+	private void updateAnswer(
+			final SQLiteDatabase db,
+			final String question,
+			final String answerNo,
+			final String text) {
+
+		final ContentValues upd = new ContentValues();
+		upd.put(ANSWER, text);
+		db.update(ANSWER, upd, ANSWER_QUERY, new String[]{question, answerNo});
 	}
 }
